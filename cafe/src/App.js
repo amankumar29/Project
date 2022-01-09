d7044b84e9c42fa55f789adfef82aeb7e650724b
codewithhappy <mannukumaraman@gmail.com>
2022-01-09 20:17:20 +0530

    ok

--- cafe/src/App.js
+++ cafe/src/App.js
@@ -3,19 +3,23 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Component/navbar/Home";
 import Login from "./Component/navbar/Login";
 import Cart from "./Component/navbar/Cart";
+import Slider from "./Component/Slider";
+import banner from "./Data/data.json";
 
 function App() {
   return (
     <>
+    {/* <h6>{JSON.stringify(banner)}</h6> */}
+    
     <Router>
     <Navbar />
-    
     <Routes>
       <Route path="/Home" element={<Home />}/>
       <Route path="/Login" element={<Login/>}/>
       <Route path="/Cart" element={<Cart/>}/>
     </Routes>
     </Router>
+    <Slider banner = {banner} />
     
     </>
   );
--- cafe/src/Component/Slider.jsx
+++ cafe/src/Component/Slider.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
-import banner1 from "../Component/img/banner/banner 1.jpg";
-import banner2 from "../Component/img/banner/banner 2.jpg";
-import banner3 from "../Component/img/banner/banner 3.jpg";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 
-const Slider = () => {
-    return (
-        <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active" data-interval="10000">
-      <img src={banner1} className="d-block w-100 " alt="..."/>
-    </div>
-    <div className="carousel-item" data-interval="2000">
-      <img src={banner2} className="d-block w-100 " alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src={banner3} className="d-block w-100 " alt="..."/>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-target="#carouselExampleInterval" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-target="#carouselExampleInterval" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </button>
-</div>
+
+
+const Slider = (props) => {
+    // console.log(props)
+    
+    return ( 
+        <>
+        {/* <img src="./img/banner/banner1.jpg" alt="pizza" /> */}
+        {/* <h6>{JSON.stringify(props.banner)}</h6> */}
+        <Carousel fade>
+            {props.banner.start.map((item)=>(
+                <Carousel.Item>
+                    <img
+                     className="d-block w-100"
+                     src={item}
+                     alt='slider'
+                     />
+                     
+              </Carousel.Item>
+            ))}
+       
+        </Carousel>
+        </>
     )
 }
 
